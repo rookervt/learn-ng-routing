18621b42dda98275d7059b9f6f0b91f1451be76f
Nelson Rooker <rookervt@gmail.com>
2018-06-27 16:29:01 -0400

    Add header navigation; add remaining site page components; route to all components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent} from './about/about.component';
+import { MembersComponent} from './members/members.component';
+import { ProjectsComponent} from './projects/projects.component';
+import { EventsComponent} from './events/events.component';
+import { ResourcesComponent} from './resources/resources.component';
+import { StartComponent} from './start/start.component';
+import { ContactComponent} from './contact/contact.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomepageComponent },
-  { path: 'about', component: AboutComponent}
+  { path: 'about', component: AboutComponent},
+  { path: 'members', component: MembersComponent},
+  { path: 'projects', component: ProjectsComponent},
+  { path: 'events', component: EventsComponent},
+  { path: 'resources', component: ResourcesComponent},
+  { path: 'start', component: StartComponent},
+  { path: 'contact', component: ContactComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,24 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { MembersComponent } from './members/members.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { EventsComponent } from './events/events.component';
+import { StartComponent } from './start/start.component';
+import { ContactComponent } from './contact/contact.component';
+import { ResourcesComponent } from './resources/resources.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    AboutComponent
+    AboutComponent,
+    MembersComponent,
+    ProjectsComponent,
+    EventsComponent,
+    StartComponent,
+    ContactComponent,
+    ResourcesComponent
   ],
   imports: [
     BrowserModule,
